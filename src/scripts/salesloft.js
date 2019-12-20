791740466fe4db220f343b23c8d55cc07b13635d
Max Baun <max@d3applications.com>
2019-12-20 10:44:21 -0500

    typeof window check

--- src/scripts/salesloft.js
+++ src/scripts/salesloft.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 export default class Salesloft extends Component {
 	render() {
-		if (window.slscout) {
+		if (typeof window !== 'undefined' && window.slscout) {
 			console.log('salesloft init');
 			window.slscout(['init', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0IjoxMTg2OH0.4ZTLQ-jBLV9Jn3YY8327OzlZuEHJ_w2oLWILwrzsU8I']);
 		}
